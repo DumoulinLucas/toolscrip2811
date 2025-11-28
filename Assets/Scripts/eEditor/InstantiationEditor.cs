7611d7c73accf84174cf6b9f306dba388ca0a7b2
DumoulinLucas <lucas.dumoulin@student.hepl.be>
2025-11-28 15:29:17 +0100

    create editor

---
+++ Assets/Scripts/eEditor/InstantiationEditor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(InstantiationControler))]
+public class InstantiationEditor : Editor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
